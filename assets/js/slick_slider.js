29c637ab9a432290bcf51e683f7e09c5072587e2
Trong Binh <trongbinh12366@gmail.com>
2022-08-15 17:27:37 +0700

    first commit

---
+++ assets/js/slick_slider.js
@@ -0,0 +1,59 @@
+$(".slider-header").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  dots: true,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  pauseOnHover: false,
+  pauseOnFocus: false,
+});
+
+$(".header").on(
+  "beforeChange",
+  function (event, slick, currentSlide, nextSlide) {
+    $(".header .slick-dots li")
+      .removeClass("slick-active")
+      .attr("aria-hidden", "true");
+    $(".header .slick-dots li button").focus(function () {
+      this.blur();
+    });
+  }
+);
+
+$(".slider-section-hot-deals").slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: false,
+  dots: false,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  pauseOnHover: false,
+  pauseOnFocus: false,
+});
+
+
+$(".slider-review").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  dots: true,
+  autoplay: true,
+  autoplaySpeed: 4000,
+  pauseOnHover: false,
+  pauseOnFocus: false,
+});
+
+$(".box_rating").on(
+  "beforeChange",
+  function (event, slick, currentSlide, nextSlide) {
+    $(".box_rating .slick-dots li")
+      .removeClass("slick-active")
+      .attr("aria-hidden", "true");
+    $(".box_rating .slick-dots li button").focus(function () {
+      this.blur();
+    });
+  }
+);
