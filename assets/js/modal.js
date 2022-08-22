6aee6bd24bad10271389ad3c4b7a1111e93a2d3e
Trong Binh <trongbinh12366@gmail.com>
2022-08-22 16:15:14 +0700

    final33

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,3 +1,23 @@
 $('#myModal').on('shown.bs.modal', function () {
     $('#myInput').trigger('focus')
   })
+
+
+  $('.decrease,.increase').on('click', function (e) {
+    e.preventDefault();
+    var inputQty = $(this).parent().find('input');
+    var currentVal = parseInt(inputQty.val());
+    var steps = parseInt(inputQty.attr('step'));
+    if ($(this).hasClass('increase')) {
+      if (currentVal <= 0 || currentVal <= inputQty.attr('min')) {
+        return;
+      }
+      currentVal = currentVal - steps;
+    } else {
+      if (currentVal >= inputQty.attr('max') && inputQty.attr('max') !=='') {
+        return;
+      }
+      currentVal = currentVal + steps;
+    }
+    inputQty.val(currentVal);
+  });
