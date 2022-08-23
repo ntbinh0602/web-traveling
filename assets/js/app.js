61de97f4c35b92695888896ba3c1fa346cc02308
Trong Binh <trongbinh12366@gmail.com>
2022-08-23 11:42:16 +0700

    abcxyz

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+
+
+// $(".heading-clicked").click(function(){
+//     $(".description-infor-detail-txt").toggleClass("hide-content-p");
+//   });
+
+
+// let btnsClick = document.querySelectorAll('.heading-clicked')
+
+// const elements = document.getElementsByClassName('.description-infor-detail-txt')
+
+
+// for (const btnClick of btnsClick) {
+//     btnClick[0].addEventListener('click', () => {
+//         console.log(element)
+//     })
+// }
