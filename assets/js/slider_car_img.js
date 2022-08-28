650b4c5a816d0bed768f0581a6b0117b8723faa4
Binh Nguyen <trongbinh12366@gmail.com>
2022-08-28 20:01:00 +0700

    sap hoan thanh

--- assets/js/slider_car_img.js
+++ assets/js/slider_car_img.js
@@ -1,20 +1,32 @@
-$('.slider-for').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    asNavFor: '.slider-nav'
+$(".slider-for").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: ".slider-nav",
 });
-$('.slider-nav').slick({
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    vertical:true,
-    asNavFor: '.slider-for',
-    dots: false,
-    focusOnSelect: true,
-    verticalSwiping:true,
-    arrows: false,
-    autoplay: true,
-    pauseOnFocus:false,
-    autoplaySpeed: 5000,
+$(".slider-nav").slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  vertical: true,
+  asNavFor: ".slider-for",
+  dots: false,
+  focusOnSelect: true,
+  verticalSwiping: true,
+  arrows: false,
+  autoplay: true,
+  pauseOnFocus: false,
+  autoplaySpeed: 5000,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: false,
+        vertical: false,
+        verticalSwiping: false,
+      },
+    },
+  ],
 });
